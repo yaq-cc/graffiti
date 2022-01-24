9c4dd90e06612eb33358ae77b4595dc7ac5bf504
Yvan J. Aquino <yvan.aquino@cloud-colosseum.net>
2022-01-24 12:40:27 +0000

    Cloud Build CI / CD

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"context"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/yaq-cc/graffiti/cache"
 	"github.com/yaq-cc/graffiti/handlers"
@@ -23,20 +25,28 @@ func main() {
 	if port == "" {
 		port = "8081"
 	}
-
-	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	parent := context.Background()
+	notify, stop := signal.NotifyContext(parent, syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
-	client, err := firestore.NewClient(ctx, project)
+	client, err := firestore.NewClient(parent, project)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	var tc cache.TemplateCache
-	tc.Listen(ctx, client) // Wait until cache gets loaded.
-
-	http.HandleFunc("/test_endpoint_1", handlers.TestEndpoint1Handler(&tc))
-	http.HandleFunc("/test_endpoint_2", handlers.TestEndpoint2Handler(&tc))
-	http.ListenAndServe(":"+port, nil)
-
-	<-ctx.Done() // Allow closing of the server when context is done.
+	// Terminate tc.Listen on signal notification.  
+	tc.Listen(notify, client) // Wait until cache gets loaded.
+	// ~~ HTTP Server ~~ //
+	mux := http.NewServeMux()
+	mux.HandleFunc("/test_endpoint_1", handlers.TestEndpoint1Handler(&tc))
+	mux.HandleFunc("/test_endpoint_2", handlers.TestEndpoint2Handler(&tc))
+	server := &http.Server{
+		Addr:        ":" + port,
+		BaseContext: func(net.Listener) context.Context { return parent },
+	}
+	go server.ListenAndServe()
+	<-notify.Done() // Allow closing of the server when context is done.
+	shutCtx, cancel := context.WithTimeout(parent, 5*time.Second)
+	defer cancel()
+	server.Shutdown(shutCtx)
 }
